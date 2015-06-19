d0b4ae6ef701d0b60956f871dc3bed55d4cfbf47
Jose E. Nunez <je.nunez@yahoo.ca>
2015-06-19 00:31:29 -0400

    list all possible command line options and their help-context of all the WEKA java classes

---
+++ list_all_possible_command_line_options_and_help_of_all_WEKA_classes.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Supposing that the "weka.jar" file is in the current directory
+
+export CLASSPATH="$CLASSPATH:weka.jar"
+unset DISPLAY
+for weka_java_class in $( jar -tf weka.jar | grep  '\.class$'   | sed '/\$/d;  s/\.class$//; s#/#.#g'  )
+do 
+      echo "Showing WEKA class $weka_java_class" 
+      java  "$weka_java_class"  -h  </dev/null
+done
+
+
+
